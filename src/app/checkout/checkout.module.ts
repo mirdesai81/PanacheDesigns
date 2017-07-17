727ba20547b9fe7fc4556687ac5d127b72d3b830
Mihir Desai <mirdesai81@gmail.com>
2017-07-16 19:58:38 -0700

    checkout component changes

--- src/app/checkout/checkout.module.ts
+++ src/app/checkout/checkout.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import {FormsModule,ReactiveFormsModule} from '@angular/forms'
 import { CheckoutViewComponent } from './checkout-view.component';
 import {RouterModule} from '@angular/router';
+import {NavbarModule} from "../navbar/navbar.module";
+import {SharedModule} from "../shared/shared.module";
 
 @NgModule({
   imports: [
-    FormsModule,ReactiveFormsModule,CommonModule,RouterModule
+    FormsModule,ReactiveFormsModule,CommonModule,RouterModule,SharedModule
   ],
   declarations: [CheckoutViewComponent],
   exports : [CheckoutViewComponent]
