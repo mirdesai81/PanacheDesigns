482a5c106f91928648101296576c096d7b9927c2
Mihir Desai <mirdesai81@gmail.com>
2017-07-19 18:01:08 -0700

    Add Toast Notification service using angular2-notifications

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import {SimpleNotificationsComponent} from 'angular2-notifications';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title : string = 'Groceries to Buy';
+
+  public options = {
+    position: ["bottom", "right"],
+    timeOut: 1500,
+    lastOnBottom: true,
+    showProgressBar: true,
+    pauseOnHover: false,
+    clickToClose: false,
+    theClass : "notifications"
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule,ReactiveFormsModule,NgForm,NgModel,NgModelGroup } from '@angular/forms';
 import { HttpModule } from '@angular/http';
@@ -11,19 +12,21 @@ import {ProductModule} from './products/product.module';
 import {routing} from "./app.routes";
 import {HomeComponent} from './home/home.component';
 import {CartModule} from './cart/cart.module';
-import {RegisterModule} from './admin/register.module';
 import {PhoneValidator} from "./shared/phone.validator";
 import {ZipValidator} from "./shared/zip.validator";
 import {EmailValidator} from "./shared/email.validator";
 import {CheckoutModule} from './checkout/checkout.module';
 import {NavbarModule} from "./navbar/navbar.module";
 import {HomeModule} from "./home/home.module";
-
+import {LoginModule} from './login/login.module';
+import {LoggedInGuard} from "./guard/auth.guard";
+import {SimpleNotificationsModule} from 'angular2-notifications';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
     HttpModule,
     routing,
@@ -31,11 +34,12 @@ import {HomeModule} from "./home/home.module";
     CategoryModule,
     ProductModule,
     CartModule,
-    RegisterModule,
     CheckoutModule,
-    HomeModule
+    HomeModule,
+    LoginModule,
+    SimpleNotificationsModule.forRoot()
   ],
-  providers: [{provide : LocationStrategy, useClass : PathLocationStrategy}],
+  providers: [{provide : LocationStrategy, useClass : PathLocationStrategy}, LoggedInGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
