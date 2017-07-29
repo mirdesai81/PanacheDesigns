070ecd139d406b37ed9bd4b2c679a47b67fd8585
Mihir Desai <mirdesai81@gmail.com>
2017-07-28 22:37:53 -0700

    Modify category schema to include parent , ancestors and children reference

--- server/controllers/category.ts
+++ server/controllers/category.ts
@@ -3,4 +3,47 @@ import BaseCtrl from "./base";
 export default class CategoryCtrl extends BaseCtrl {
   model = Category;
   identityField = 'categoryId';
+
+  // Get all
+  getAll = (req, res) => {
+    this.model.find({}).populate('parent ancestors children').exec((err,docs) => {
+      if (err) { console.error(err); throw err; }
+      console.log(docs);
+      res.json(docs);
+    });
+  };
+
+  insert = (req, res) => {
+    var parent = req.body.parent || null;
+
+    if(parent) {
+      this.model.findOne({_id : parent}).then( category => {
+        console.log(category);
+        return category.addChild(req.body);
+      }).then(child => {
+        return res.status(200).json(child);
+      }).catch(err => {
+        if (err && err.code === 11000) {
+          res.status(400).json({success : false, message : "Cannot insert duplicate key for category"});
+        }
+        if (err) {
+          console.log(err);
+          res.status(400).json({success : false, message : "Cannot insert category"});
+        }
+      });
+    } else {
+      const obj = new this.model(req.body);
+      obj.save((err, item) => {
+        // 11000 is the code for duplicate key error
+        if (err && err.code === 11000) {
+          res.status(400).json({success : false, message : "Cannot insert duplicate key for category"});
+        }
+        if (err) {
+          console.log(err);
+          res.status(400).json({success : false, message : "Cannot insert category"});
+        }
+        res.status(200).json(item);
+      });
+    }
+  };
 }
--- server/models/category.ts
+++ server/models/category.ts
@@ -1,5 +1,6 @@
 import * as mongoose from 'mongoose';
 import * as autoIncrement from 'mongoose-sequence';
+import * as slugs from 'mongoose-url-slugs';
 
 /*{id: '1', title: 'Bread & Bakery',
   imageL: 'https://placeholdit.imgix.net/~text?txtsize=33&bg=373a3c&txtclr=ffffff&txt=1110%C3%97350&w=1100&h=350',
@@ -7,13 +8,37 @@ import * as autoIncrement from 'mongoose-sequence';
   desc: 'The best cupcakes, cookies, cakes, pies, cheesecakes, fresh bread, biscotti, muffins, bagels, fresh coffee and more.' }*/
 
 const categorySchema = new mongoose.Schema({
-  title : { type : String, unique : true},
+  categoryId : {type : Number},
+  title : { type : String, trim : true},
   imageL : String,
   imageS : String,
-  desc : String
+  desc : String,
+  parent : {type : mongoose.Schema.Types.ObjectId , ref : 'Category'},
+  ancestors : [{type : mongoose.Schema.Types.ObjectId , ref : 'Category'}],
+  children : [{type : mongoose.Schema.Types.ObjectId, ref : 'Category'}]
 },{collection : 'Category'});
 
+categorySchema.methods = {
+  addChild : function(child) {
+    var that = this;
+    child.parent = this._id;
+    child.ancestors = this.ancestors.concat([this._id]);
+    return this.model('Category').create(child,function(err,children){
+
+        if (err) {
+          console.log(err);
+          /*res.status(400).json({success : false, message : "Cannot update the parent category"});*/
+        }
+      console.log(children);
+      that.children.push(children._id);
+      that.save();
+    });
+  }
+};
+
 categorySchema.plugin(autoIncrement,{ inc_field : 'categoryId'});
+categorySchema.plugin(slugs('title'));
+
 
 
 categorySchema.pre('save',function(next){
